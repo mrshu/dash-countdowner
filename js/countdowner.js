aa5a22a356bf0e549ce3e0ce8b7e1e3311b7b760
mr.Shu <mr@shu.io>
2014-03-13 01:05:15 +0100

    finish text

--- js/countdowner.js
+++ js/countdowner.js
@@ -2,8 +2,17 @@ function CountDowner(options) {
     this.element = options.element || "";
     this.time = options.time || 3600 * 1000;
     this.interval = options.interval || -50;
+    this.callback = options.callback || function(){};
 }
 CountDowner.prototype.start = function() {
+    var $this = this;
+
     $(this.element).stopwatch({startTime: this.time, updateInterval: this.interval})
                     .stopwatch('start');
+    $(this.element).stopwatch().bind('tick.stopwatch', function(event, millis){
+        if (millis == 0) {
+            $($this.element).stopwatch('stop');
+            $this.callback();
+        }
+    });
 }
